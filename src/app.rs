85ee63bc148bc3ab4044e3176a673a662e557ccc
Thomas Gale <thomas.gale@autodesk.com>
2023-09-25 19:02:27 +0100

    Don't overwrite default font.

--- src/app.rs
+++ src/app.rs
@@ -24,7 +24,7 @@ fn Home() -> impl IntoView {
     view! {
         <Title text="Leptos + Tailwindcss"/>
         <main>
-            <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
+            <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white flex flex-col min-h-screen">
                 <div class="flex flex-row-reverse flex-wrap m-auto">
                     <button on:click=move |_| set_value.update(|value| *value += 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
                         "+"
