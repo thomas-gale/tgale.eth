ac0507aa35aeb10546edea17b3f49ff5c6f77861
Thomas Gale <thomas.gale@autodesk.com>
2023-09-25 11:39:30 +0100

    WIP: Starting on a Leptos migration.
    TODO: The CI will break till the toolchain is updated to use rust nightly.

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,38 @@
-use yew::prelude::*;
+// use yew::prelude::*;
 
-use super::feed;
-use super::footer;
-use super::side_bar;
+// use super::feed;
+// use super::footer;
+// use super::side_bar;
 
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <main>
-            <div class="flex flex-col h-full">
-                <div class="flex flow-row flex-wrap ju h-full">
-                    <side_bar::SideBar/>
-                    <feed::Feed/>
-                </div>
-                <footer::Footer/>
-            </div>
-        </main>
+// #[function_component(App)]
+// pub fn app() -> Html {
+//     html! {
+//         <main>
+//             <div class="flex flex-col h-full">
+//                 <div class="flex flow-row flex-wrap ju h-full">
+//                     <side_bar::SideBar/>
+//                     <feed::Feed/>
+//                 </div>
+//                 <footer::Footer/>
+//             </div>
+//         </main>
+//     }
+// }
+
+use leptos::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count(3);
+            }
+        >
+            "Click me: "
+            {move || count()}
+        </button>
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-mod app;
-mod feed;
-mod footer;
-mod side_bar;
+// mod app;
+// mod feed;
+// mod footer;
+// mod side_bar;
 
+// use app::App;
+
+// fn main() {
+//     yew::start_app::<App>();
+// }
+
+use leptos::*;
+
+mod app;
 use app::App;
 
 fn main() {
-    yew::start_app::<App>();
+    mount_to_body(|| view! { <App/> })
 }
